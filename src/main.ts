caf9477ecf9fcbd12f9c27f6a146481ba571b682
TitoRovi <48201628+roviralta@users.noreply.github.com>
2022-08-25 08:41:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,6 @@ import "@ionic/vue/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 import "./theme/modals.css";
-import 'animate.css';
 
 import { defineCustomElements } from "@ionic/pwa-elements/loader";
 
